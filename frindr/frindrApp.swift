d6d8ba2607151507bab3d15546c769f2fedbf010
stormingluke <luke.c3@pm.me>
2026-01-17 22:47:06 +0100

    Initial Commit

---
+++ frindr/frindrApp.swift
@@ -0,0 +1,17 @@
+//
+//  frindrApp.swift
+//  frindr
+//
+//  Created by Luke Caradine on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct frindrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
